6ff003b35a7b51a711da1537e7b9dfc405aa2378
gfeiden <gregory.a.feiden.gr@dartmouth.edu>
2014-07-15 21:08:49 +0200

    changed to log10 arithmetic

--- js/polytrope.js
+++ js/polytrope.js
@@ -8,12 +8,27 @@ var Constants = {
     Msun: 1.989e33,      // solar mass in g
     Rsun: 6.956e10,      // solar radius in cm
     Lsun: 3.846e33,      // solar luminosity in erg/s
+
+    logMsun: 33.2986348, // log10(Msun)
+    logRsun: 10.8424596, // log10(Rsun)
+    logLsun: 33.5850093, // log10(Lsun)
     
     // Physical constants
     m_proton: 1.672e-24, // mass of proton (g)
     G: 6.673e-8,         // Newton's gravitational G
     k_boltz: 1.381e-16,  // Boltzmann constant
-    sigma: 5.671e-5      // Stefan-Boltzmann constant
+    sigma: 5.671e-5,     // Stefan-Boltzmann constant
+
+    logmp: -23.7767637,  // log10(proton mass)
+    logG: -7.1756789,    // log10(Newton G)
+    logk: -15.8598063,   // log10(k)
+    logs: -4.2463404,    // log10(sigma)
+
+    log4p: log10(4.0*Math.PI)
+}
+
+function log10(x) {
+    return Math.log(x)/Math.log(10.0);
 }
 
 function eos(Star, kind) {
@@ -22,34 +37,33 @@ function eos(Star, kind) {
 
     if (kind == "degen") {
         var gamma = 4.0/3.0;
-    }
-    else {
+    } else {
         var gamma = 5.0/3.0;
     }
     
     // define for convenience
-    var Mtot = Star.mass*Constants.Msun;
-    var Rtot = Star.radius*Constants.Rsun;
+    var Mtot = log10(Star.mass) + Constants.logMsun;
+    var Rtot = Star.radius + Constants.logRsun;
     
-    // central thermodynamic variables
-    var M = [0.0];
-    var rho = [-Mtot*Star.zero*Math.pow(Star.slope, -1)/(4.0*Math.PI*Math.pow(Rtot, 3))];
-    var P = [Math.pow(Rtot/Star.zero, 2)*(4.0*Math.PI*Constants.G*Math.pow(rho[0], 2))/(Star.n + 1.0)];
-    var K = P[0]/Math.pow(rho[0], gamma);
-    var R = [Math.sqrt(((Star.n + 1.0)*P[0])/(4.0*Math.PI*Constants.G*Math.pow(rho[0], 2)))*Star.x[0]];
+    // central thermodynamic variables (all in log10 form)
+    var M = [-10.0]; 
+    var rho = [Mtot + log10(Star.zero) - log10(-1.0*Star.slope) - Constants.log4p - 3.0*Rtot];
+    var P = [2.0*(Rtot - log10(Star.zero)) + Constants.log4p + Constants.logG + 2.0*rho[0] - log10(Star.n + 1.0)];
+    var K = P[0] - gamma*rho[0];
+    var R = [0.5*(P[0] + log10(Star.n + 1.0) - Constants.log4p - Constants.logG - 2.0*rho[0]) + log10(Star.x[0])];
 
-    if (kind === "ideal") {
-        var T = [Constants.m_proton*P[0]/(Star.mu*Constants.k_boltz*rho[0])];
+    if (kind == "ideal") {
+        var T = [Constants.logmp + P[0] - log10(Star.mu) - Constants.logk - rho[0]];
         for (i = 1; i < Star.y.length - 1; i++) {
-            rho[i] = rho[0]*Math.pow(Star.y[i], Star.n);
-            P[i] = K*Math.pow(rho[i], 1.0/Star.n + 1.0);
-            T[i] = Constants.m_proton*P[i]/(Star.mu*Constants.k_boltz*rho[i]);
-            R[i] = Math.sqrt(((Star.n + 1.0)*P[i])/(4.0*Math.PI*Constants.G*Math.pow(rho[i], 2)))*Star.x[i];
-            M[i] = M[i - 1] + 4.0*Math.PI*Math.pow(R, 2)*rho[i]*(R[i] - R[i - 1]);
+            rho[i] = rho[0] + Star.n*log10(Star.y[i]);
+            P[i] = K + (1.0/Star.n + 1.0)*rho[i];
+            T[i] = Constants.logmp + P[i] - log10(Star.mu) - Constants.logk - rho[i];
+            R[i] = 0.5*(log10(Star.n + 1.0) + P[i] - Constants.log4p - Constants.logG - 2.0*rho[i]) + log10(Star.x[i]);
+            M[i] = Constants.log4p + 2.0*R[i] + rho[i] + log10(Math.pow(10.0, R[i]).toExponential() - Math.pow(10.0, R[i - 1]).toExponential()); 
         }
     }
     else if (kind == "degen") {
-        T = [0.0];
+        var T = [0.0];
         for (i = 1; i < Star.y.length - 1; i++) {
             rho[i] = rho[0]*Math.pow(y[i], Star.n);
             P[i] = K*Math.pow(rho[i], 1.0/Star.n + 1.0);
@@ -59,11 +73,11 @@ function eos(Star, kind) {
         }
     }
     else {
-        rho = ["Incorrect EOS"];
-        P = ["Incorrect EOS"];
-        T = ["Incorrect EOS"];
-        R = ["Incorrect EOS"];
-        M = ["Incorrect EOS"];
+        var rho = ["Incorrect EOS"];
+        var P = ["Incorrect EOS"];
+        var T = ["Incorrect EOS"];
+        var R = ["Incorrect EOS"];
+        var M = ["Incorrect EOS"];
     }
     return [rho, P, T, R, M];
 }
@@ -114,13 +128,13 @@ function starInit(mass, teff, luminosity, n, X, Y, Z) {
         mass: mass,
         teff: teff,
         luminosity: luminosity,
-        radius: Math.sqrt(luminosity*Constants.Lsun/(4.0*Math.PI*Constants.sigma*Math.pow(teff, 4)))/Constants.Rsun,
+        radius: 0.5*(log10(luminosity) + Constants.logLsun - Constants.log4p - Constants.logs - 4.0*log10(teff)) - Constants.logRsun,
         n: n,
         X: X,
         Y: Y,
-        Z: Z,
-        mu: 1.0/(2.0*X + 0.75*Y + 0.5*Z)
+        Z: Z
     }
+    Star.mu = 1.0/(2.0*X + 0.75*Y + 0.5*Z);
     Star.rk4_x = [0.001];
     Star.rk4_y = [1.0 - Math.pow(Star.rk4_x[0], 2)/6.0];
     Star.rk4_z = [-Star.rk4_x[0]/3.0];
@@ -142,7 +156,8 @@ function polytrope(mass, teff, luminosity, n, Y, Z, h, eosKind) {
     h = h || 0.001;
     eosKind = eosKind || "ideal";
     
-    var Star = starInit(mass, teff, luminosity, n, Y, Z);
+    var X = 1.0 - Y - Z;
+    var Star = starInit(mass, teff, luminosity, n, X, Y, Z);
     var solution = solveRK4(Star.rk4_x, Star.rk4_y, Star.rk4_z, Star.rk4_dz, h, n);
     Star.x = solution[0];
     Star.y = solution[1];
@@ -154,6 +169,7 @@ function polytrope(mass, teff, luminosity, n, Y, Z, h, eosKind) {
     Star.slope = (Star.y[len - 2] - Star.y[len - 1])/(Star.x[len - 2] - Star.x[len - 1]);
     Star.zero = Star.x[len - 1] - Star.y[len - 1]/Star.slope;
 
+    // convert dimensionless quantities to physical units
     var props = eos(Star, eosKind);
     Star.rho = props[0];
     Star.P = props[1];
